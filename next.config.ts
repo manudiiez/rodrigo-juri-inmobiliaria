95eb03179a9f2251b03056ec34b6f8f1c66c7be3
Manuel Diez de Oñate <manudiiez@MacBook-Pro-de-Manuel.local>
2025-11-28 12:52:51 -0300

    nextconfig ready

--- next.config.ts
+++ next.config.ts
@@ -4,21 +4,31 @@ import createNextIntlPlugin from "next-intl/plugin";
 // Plugin de next-intl apuntando a la configuración de routing
 const withNextIntl = createNextIntlPlugin();
 
-const isProduction = !process.env.NODE_ENV || process.env.NODE_ENV === 'production';
-
+const isProduction = false;
+// const isProduction = !process.env.NODE_ENV || process.env.NODE_ENV === 'production';
 // Configurar basePath y assetPrefix según el entorno
 const basePath = isProduction ? "/rodrigo-juri-inmobiliaria" : "";
 const assetPrefix = isProduction ? "/rodrigo-juri-inmobiliaria" : "";
 
 // Mostrar configuración en consola
-console.log('\n🔧 Configuración de Next.js:');
-console.log(`   Entorno: ${isProduction ? '🚀 Production' : '🔨 Development'}`);
-console.log(`   basePath: "${basePath || '(vacío)'}"`);
-console.log(`   assetPrefix: "${assetPrefix || '(vacío)'}"\n`);
+// console.log('\n🔧 Configuración de Next.js:');
+// console.log(`   Entorno: ${isProduction ? '🚀 Production' : '🔨 Development'}`);
+// console.log(`   basePath: "${basePath || '(vacío)'}"`);
+// console.log(`   assetPrefix: "${assetPrefix || '(vacío)'}"\n`);
 
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Aquí puedes poner tus otras configuraciones globales si tienes */
+  reactStrictMode: true,
+
+  // 2. Aquí está la lógica condicional:
+  ...(isProduction ? {
+    output: "export",
+    images: { unoptimized: true },
+    basePath: basePath,
+    assetPrefix: assetPrefix,
+  } : {}), // <--- Si no es producción, agrega un objeto vacío (nada)
+
 };
 
 export default withNextIntl(nextConfig);
